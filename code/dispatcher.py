b70b64c407b7110e5de94c8e4822ad1b956c0b2a
Nikita Rvachev <nikita@rvachev.org>
2023-02-22 17:20:04 -0500

    Update log message for joining message

--- code/dispatcher.py
+++ code/dispatcher.py
@@ -26,7 +26,8 @@ async def tg_new_member(update, context):
     
     if delete_NEW_CHAT_MEMBERS_message == True:
         await bot.delete_message(update.message.chat.id,update.message.id)
-        admin_log(f"Deleted {update.message.id} message from {update.message.chat.id} chat ")
+
+        admin_log(f"Joining message deleted from chat {update.message.chat.title} ({update.message.chat.id}) for user {update.message.from_user.username} ({update.message.from_user.id})
 
 async def wiretapping(update, context):
     #check if chat id is the same as in config
