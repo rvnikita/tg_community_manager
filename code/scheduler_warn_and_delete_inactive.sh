1a65d0f7d0d802b5c2c7630c0eeb1508c378ee43
Nikita Rvachev <nikita@rvachev.org>
2023-02-22 16:47:54 -0500

    Scheduler for users_warn_and_delete_inactive to run it on the 1st day of month. We will run this scheduler every day

---
+++ code/scheduler_warn_and_delete_inactive.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Get the current day of the month
+day=$(date +%d)
+
+# If the current day is 1, start the Python script
+if [ $day -eq 1 ]; then
+    python3 users_warn_and_delete_inactive.py
+fi
