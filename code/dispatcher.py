d81a1520a3cf978e79214e0638cfec5be9492496
Nikita Rvachev <nikita@rvachev.org>
2023-02-22 15:14:44 -0500

    Join despatcher & wiretapping together, because it could be only one dispatcher.

--- code/dispatcher.py
+++ code/dispatcher.py
@@ -4,6 +4,9 @@ import configparser
 from telegram import Bot
 from telegram.ext import Application, MessageHandler, filters
 
+from datetime import datetime
+import psycopg2
+
 config = configparser.ConfigParser()
 config_path = os.path.dirname(os.path.dirname(__file__)) + '/config/' #we need this trick to get path to config folder
 config.read(config_path + 'settings.ini')
@@ -25,15 +28,54 @@ async def tg_new_member(update, context):
         await bot.delete_message(update.message.chat.id,update.message.id)
         admin_log(f"Deleted {update.message.id} message from {update.message.chat.id} chat ")
 
-    
-async def tg_text(update, context):
-    return
+async def wiretapping(update, context):
+    if len(update.message.new_chat_members) > 0: #user added
+        db_update_user(update.message.new_chat_members[0].id, update.message.new_chat_members[0].username, datetime.now())
+    else:
+        db_update_user(update.message.from_user.id, update.message.from_user.username, datetime.now())
+    #admin_log(f"{update.message.from_user.username} ({update.message.from_user.id}): {update.message.text}")
+
+
+def db_update_user(user_id, username, last_message_datetime):
+    #TODO: we need to relocate this function to another location
+    conn = None
+    try:
+        conn = psycopg2.connect(user=config['DB']['DB_USER'],
+                                password=config['DB']['DB_PASSWORD'],
+                                host=config['DB']['DB_HOST'],
+                                port=config['DB']['DB_PORT'],
+                                database=config['DB']['DB_DATABASE'])
+
+        sql = f"""
+        UPDATE users SET username='{username}', last_message_datetime='{last_message_datetime}' WHERE id={user_id};
+        INSERT INTO users (id, username, last_message_datetime)
+               SELECT {user_id}, '{username}', '{last_message_datetime}'
+               WHERE NOT EXISTS (SELECT 1 FROM users WHERE id={user_id});
+        """
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        cur.execute(sql)
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
 
 def main() -> None:
     application = Application.builder().token(config['BOT']['KEY']).build()
-    
-    application.add_handler(MessageHandler(filters.TEXT, tg_text))
+
+    #delete new member message
     application.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, tg_new_member))
+
+    #wiretapping
+    application.add_handler(MessageHandler(filters.TEXT, wiretapping))
+    application.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, wiretapping))
 
     # Start the Bot
     application.run_polling()
