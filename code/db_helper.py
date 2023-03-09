b08d704b4cc478cd8e1391500a8be66df2c1cf1a
Nikita Rvachev <nikita@rvachev.org>
2023-03-09 12:13:28 -0500

    lib to work with db

---
+++ code/db_helper.py
@@ -0,0 +1,22 @@
+from admin_log import admin_log
+import configparser
+import os
+
+config = configparser.ConfigParser()
+config_path = os.path.dirname(os.path.dirname(__file__)) + '/config/' #we need this trick to get path to config folder
+config.read(config_path + 'db.ini')
+
+def connect():
+    conn = None
+    try:
+        conn = psycopg2.connect(user=config['DB']['DB_USER'],
+                                password=config['DB']['DB_PASSWORD'],
+                                host=config['DB']['DB_HOST'],
+                                port=config['DB']['DB_PORT'],
+                                database=config['DB']['DB_DATABASE'])
+        return conn
+    except (Exception, psycopg2.DatabaseError) as error:
+        #write admin log mentioning file name and error
+        admin_log(f"Error in {__file__}: while connecting to PostgreSQL: {error}", critical=True)
+
+        return None
