6cf1ea74ee2cf4b00ad139df8b505debf8ad803e
Nikita Rvachev <nikita@rvachev.org>
2023-03-24 12:48:42 -0400

    #FIXME: this is not working, rewrite with user_status and ORM

--- src/cron/users_warn_and_delete_inactive.py
+++ src/cron/users_warn_and_delete_inactive.py
@@ -18,63 +18,65 @@ admin_log(f"Starting {__file__} in {config['BOT']['MODE']} mode at {os.uname()}"
 bot = telegram.Bot(token=config['BOT']['KEY'])
 
 async def warn_inactive():
-    conn = None
-    try:
-        conn = psycopg2.connect(user=config['DB']['DB_USER'],
-                                password=config['DB']['DB_PASSWORD'],
-                                host=config['DB']['DB_HOST'],
-                                port=config['DB']['DB_PORT'],
-                                database=config['DB']['DB_DATABASE'])
-
-        sql = "SELECT * FROM tg_user WHERE last_message_datetime < NOW() - INTERVAL '60 days' AND last_message_datetime >= NOW() - INTERVAL '90 days' AND status IN ('member', 'administrator') AND username <> 'None';"
-        cur = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
-        cur.execute(sql)
-
-        rows = cur.fetchall()
-
-        #join all nicknames with attached @ before them in one string separated by comma and last with "и "
-        joined_nicknames = ', '.join([f"@{row['username']}" for row in rows[:-1]]) + f" и @{rows[-1]['username']}"
-
-        warn_text = "Список кандидатов-молчунов на выбывание в этом месяце " + joined_nicknames + ". Уже как минимум пару месяцев мы ничего от них не слышали :("
-
-        await bot.send_message(chat_id=config['BOT']['CHAT_ID'], text=warn_text)
-
-    except (Exception, psycopg2.DatabaseError) as error:
-        admin_log(f"Error in file {__file__}: {error}", critical=True)
-    finally:
-        if conn is not None:
-                conn.close()
+    #FIXME: this is not working, rewrite with user_status and ORM
+    # conn = None
+    # try:
+    #     conn = psycopg2.connect(user=config['DB']['DB_USER'],
+    #                             password=config['DB']['DB_PASSWORD'],
+    #                             host=config['DB']['DB_HOST'],
+    #                             port=config['DB']['DB_PORT'],
+    #                             database=config['DB']['DB_DATABASE'])
+    #
+    #     sql = "SELECT * FROM tg_user WHERE last_message_datetime < NOW() - INTERVAL '60 days' AND last_message_datetime >= NOW() - INTERVAL '90 days' AND status IN ('member', 'administrator') AND username <> 'None';"
+    #     cur = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+    #     cur.execute(sql)
+    #
+    #     rows = cur.fetchall()
+    #
+    #     #join all nicknames with attached @ before them in one string separated by comma and last with "и "
+    #     joined_nicknames = ', '.join([f"@{row['username']}" for row in rows[:-1]]) + f" и @{rows[-1]['username']}"
+    #
+    #     warn_text = "Список кандидатов-молчунов на выбывание в этом месяце " + joined_nicknames + ". Уже как минимум пару месяцев мы ничего от них не слышали :("
+    #
+    #     await bot.send_message(chat_id=config['BOT']['CHAT_ID'], text=warn_text)
+    #
+    # except (Exception, psycopg2.DatabaseError) as error:
+    #     admin_log(f"Error in file {__file__}: {error}", critical=True)
+    # finally:
+    #     if conn is not None:
+    #             conn.close()
 
 async def delete_inactive():
-    pass
-
-    conn = None
-    try:
-        conn = psycopg2.connect(user=config['DB']['DB_USER'],
-                                password=config['DB']['DB_PASSWORD'],
-                                host=config['DB']['DB_HOST'],
-                                port=config['DB']['DB_PORT'],
-                                database=config['DB']['DB_DATABASE'])
-
-        #we don't touch admins
-        sql = "SELECT * FROM tg_user WHERE last_message_datetime < NOW() - INTERVAL '90 days' AND status IN ('member') AND username <> 'None';"
-        cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-        cur.execute(sql)
-
-        rows = cur.fetchall()
-
-        for row in rows:
-            try:
-                await bot.kick_chat_member(config['BOT']['CHAT_ID'], row['id'])
-                admin_log(f"User @{row['username']} ({row['id']}) was kicked for inactivity")
-            except (Exception, psycopg2.DatabaseError) as error:
-                admin_log(f"Error in file {__file__}: {error}", critical=True)
-
-    except (Exception, psycopg2.DatabaseError) as error:
-        admin_log(f"Error in file {__file__}: {error}", critical=True)
-    finally:
-        if conn is not None:
-            conn.close()
+    # FIXME: this is not working, rewrite with user_status and ORM
+    # pass
+    #
+    # conn = None
+    # try:
+    #     conn = psycopg2.connect(user=config['DB']['DB_USER'],
+    #                             password=config['DB']['DB_PASSWORD'],
+    #                             host=config['DB']['DB_HOST'],
+    #                             port=config['DB']['DB_PORT'],
+    #                             database=config['DB']['DB_DATABASE'])
+    #
+    #     #we don't touch admins
+    #     sql = "SELECT * FROM tg_user WHERE last_message_datetime < NOW() - INTERVAL '90 days' AND status IN ('member') AND username <> 'None';"
+    #     cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    #     cur.execute(sql)
+    #
+    #     rows = cur.fetchall()
+    #
+    #     for row in rows:
+    #         try:
+    #             await bot.kick_chat_member(config['BOT']['CHAT_ID'], row['id'])
+    #             admin_log(f"User @{row['username']} ({row['id']}) was kicked for inactivity")
+    #         except (Exception, psycopg2.DatabaseError) as error:
+    #             admin_log(f"Error in file {__file__}: {error}", critical=True)
+    #
+    # except (Exception, psycopg2.DatabaseError) as error:
+    #     admin_log(f"Error in file {__file__}: {error}", critical=True)
+    # finally:
+    #     if conn is not None:
+    #         conn.close()
 
 
 async def main() -> None:
