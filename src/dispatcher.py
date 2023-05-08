de4994d3a1324a88c1b1f51e77dff02735ee50a1
Nikita Rvachev <nikita@rvachev.org>
2023-05-08 09:55:23 -0400

    Wrap tg_update_user_status in try/except block

--- src/dispatcher.py
+++ src/dispatcher.py
@@ -252,86 +252,88 @@ async def tg_new_member(update, context):
         logger.error(f"Error: {traceback.format_exc()}")
 
 async def tg_update_user_status(update, context):
-    #TODO: we need to rewrite all this to support multiple chats. May be we should add chat_id to user table
-    if update.message is not None:
-        config_update_user_status = chat_helper.get_chat_config(update.message.chat.id, "update_user_status")
-        if config_update_user_status == None:
-            logger.info(f"Skip: no config for chat {update.message.chat.id} ({update.message.chat.title})")
-            return
+    try:
+        #TODO: we need to rewrite all this to support multiple chats. May be we should add chat_id to user table
+        if update.message is not None:
+            config_update_user_status = chat_helper.get_chat_config(update.message.chat.id, "update_user_status")
+            if config_update_user_status == None:
+                logger.info(f"Skip: no config for chat {update.message.chat.id} ({update.message.chat.title})")
+                return
 
-        if config_update_user_status == True:
-            if len(update.message.new_chat_members) > 0: #user added
-                #TODO:HIGH: We need to rewrite this so we can also add full name
-                db_update_user(update.message.new_chat_members[0].id, update.message.chat.id,  update.message.new_chat_members[0].username, datetime.now(), update.message.new_chat_members[0].first_name, update.message.new_chat_members[0].last_name)
-            else:
-                # TODO:HIGH: We need to rewrite this so we can also add full name
-                db_update_user(update.message.from_user.id, update.message.chat.id, update.message.from_user.username, datetime.now(), update.message.from_user.first_name, update.message.from_user.last_name)
+            if config_update_user_status == True:
+                if len(update.message.new_chat_members) > 0: #user added
+                    #TODO:HIGH: We need to rewrite this so we can also add full name
+                    db_update_user(update.message.new_chat_members[0].id, update.message.chat.id,  update.message.new_chat_members[0].username, datetime.now(), update.message.new_chat_members[0].first_name, update.message.new_chat_members[0].last_name)
+                else:
+                    # TODO:HIGH: We need to rewrite this so we can also add full name
+                    db_update_user(update.message.from_user.id, update.message.chat.id, update.message.from_user.username, datetime.now(), update.message.from_user.first_name, update.message.from_user.last_name)
 
-            #logger.info(f"User status updated for user {update.message.from_user.id} in chat {update.message.chat.id} ({update.message.chat.title})")
+                #logger.info(f"User status updated for user {update.message.from_user.id} in chat {update.message.chat.id} ({update.message.chat.title})")
 
 
-        #TODO: we need to separate this part of the code to separate funciton tg_openai_autorespond
-        if update.message.chat.id == -1001588101140: #O1
-        # if update.message.chat.id == -1001688952630:  # debug
-            #TODO: we need to support multiple chats, settings in db etc
+            #TODO: we need to separate this part of the code to separate funciton tg_openai_autorespond
+            if update.message.chat.id == -1001588101140: #O1
+            # if update.message.chat.id == -1001688952630:  # debug
+                #TODO: we need to support multiple chats, settings in db etc
 
-            #Let's here check if we know an answer for a question and send it to user
-            openai.api_key = config['OPENAI']['KEY']
+                #Let's here check if we know an answer for a question and send it to user
+                openai.api_key = config['OPENAI']['KEY']
 
-            messages = [
-                {"role": "system",
-                 "content": f"Answer only yes or no"},
-                    {"role": "user", "content": f"Is this a question: \"{update.message.text}\""}
-            ]
+                messages = [
+                    {"role": "system",
+                     "content": f"Answer only yes or no"},
+                        {"role": "user", "content": f"Is this a question: \"{update.message.text}\""}
+                ]
 
-            response = openai.ChatCompletion.create(
-                model=config['OPENAI']['COMPLETION_MODEL'],
-                messages=messages,
-                temperature=float(config['OPENAI']['TEMPERATURE']),
-                max_tokens=int(config['OPENAI']['MAX_TOKENS']),
-                top_p=1,
-                frequency_penalty=0,
-                presence_penalty=0
-            )
+                response = openai.ChatCompletion.create(
+                    model=config['OPENAI']['COMPLETION_MODEL'],
+                    messages=messages,
+                    temperature=float(config['OPENAI']['TEMPERATURE']),
+                    max_tokens=int(config['OPENAI']['MAX_TOKENS']),
+                    top_p=1,
+                    frequency_penalty=0,
+                    presence_penalty=0
+                )
 
-            #check if response.choices[0].message.content contains "yes" without case sensitivity
-            if "yes" in response.choices[0].message.content.lower():
-                rows = openai_helper.get_nearest_vectors(update.message.text, 0)
+                #check if response.choices[0].message.content contains "yes" without case sensitivity
+                if "yes" in response.choices[0].message.content.lower():
+                    rows = openai_helper.get_nearest_vectors(update.message.text, 0)
 
-                logger.info("Question detected " + update.message.text)
+                    logger.info("Question detected " + update.message.text)
 
-                if len(rows) > 0:
-                    logger.info("Vectors detected " + str(rows) + str(rows[0]['similarity']))
+                    if len(rows) > 0:
+                        logger.info("Vectors detected " + str(rows) + str(rows[0]['similarity']))
 
-                    #TODO this is a debug solution to skip questions with high similarity
-                    if rows[0]['similarity'] < float(config['OPENAI']['SIMILARITY_THRESHOLD']):
-                        logger.info("Skip, similarity=" + str(rows[0]['similarity']) + f" while threshold={config['OPENAI']['SIMILARITY_THRESHOLD']}", critical=False)
-                        return #skip this message
+                        #TODO this is a debug solution to skip questions with high similarity
+                        if rows[0]['similarity'] < float(config['OPENAI']['SIMILARITY_THRESHOLD']):
+                            logger.info("Skip, similarity=" + str(rows[0]['similarity']) + f" while threshold={config['OPENAI']['SIMILARITY_THRESHOLD']}", critical=False)
+                            return #skip this message
 
-                    messages = [
-                        {"role": "system",
-                         "content": f"Answer in one Russian message based on user question and embedding vectors. Do not mention embedding. Be applicable and short."},
-                        {"role": "user", "content": f"\"{update.message.text}\""}
-                    ]
+                        messages = [
+                            {"role": "system",
+                             "content": f"Answer in one Russian message based on user question and embedding vectors. Do not mention embedding. Be applicable and short."},
+                            {"role": "user", "content": f"\"{update.message.text}\""}
+                        ]
 
-                    for i in range(len(rows)):
-                        messages.append({"role": "system", "content": f"Embedding Title {i}: {rows[i]['title']}\n Embedding Body {i}: {rows[i]['body']}"})
+                        for i in range(len(rows)):
+                            messages.append({"role": "system", "content": f"Embedding Title {i}: {rows[i]['title']}\n Embedding Body {i}: {rows[i]['body']}"})
 
-                    response = openai.ChatCompletion.create(
-                        model=config['OPENAI']['COMPLETION_MODEL'],
-                        messages=messages,
-                        temperature=float(config['OPENAI']['TEMPERATURE']),
-                        max_tokens=int(config['OPENAI']['MAX_TOKENS']),
-                        top_p=1,
-                        frequency_penalty=0,
-                        presence_penalty=0
-                    )
-                    await bot.send_message(update.message.chat.id, response.choices[0].message.content + f" ({rows[0]['similarity']:.2f})", reply_to_message_id=update.message.message_id)
+                        response = openai.ChatCompletion.create(
+                            model=config['OPENAI']['COMPLETION_MODEL'],
+                            messages=messages,
+                            temperature=float(config['OPENAI']['TEMPERATURE']),
+                            max_tokens=int(config['OPENAI']['MAX_TOKENS']),
+                            top_p=1,
+                            frequency_penalty=0,
+                            presence_penalty=0
+                        )
+                        await bot.send_message(update.message.chat.id, response.choices[0].message.content + f" ({rows[0]['similarity']:.2f})", reply_to_message_id=update.message.message_id)
 
-                    #resend update.message to admin
-                    await bot.forward_message(config['BOT']['ADMIN_ID'], update.message.chat.id, update.message.message_id)
-                    await bot.send_message(config['BOT']['ADMIN_ID'], response.choices[0].message.content + f" ({rows[0]['similarity']:.2f})", disable_web_page_preview=True)
-
+                        #resend update.message to admin
+                        await bot.forward_message(config['BOT']['ADMIN_ID'], update.message.chat.id, update.message.message_id)
+                        await bot.send_message(config['BOT']['ADMIN_ID'], response.choices[0].message.content + f" ({rows[0]['similarity']:.2f})", disable_web_page_preview=True)
+    except Exception as e:
+        logger.error(f"Error: {traceback.format_exc()}")
 
 
 def db_update_user(user_id, chat_id, username, last_message_datetime, first_name=None, last_name=None):
