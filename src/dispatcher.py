50f4935625a974e4b6221cf964ed13b905f17d58
Nikita Rvachev <nikita@rvachev.org>
2023-06-19 21:20:50 -0400

    rename welcome_message to welcome_dm_message

--- src/dispatcher.py
+++ src/dispatcher.py
@@ -226,10 +226,10 @@ async def tg_thankyou(update, context):
 
 async def tg_join_request(update, context):
     try:
-        welcome_message = chat_helper.get_chat_config(update.effective_chat.id, "welcome_message")
+        welcome_dm_message = chat_helper.get_chat_config(update.effective_chat.id, "welcome_dm_message")
 
-        if welcome_message is not None and welcome_message != "":
-            await bot.send_message(update.effective_user.id, welcome_message, disable_web_page_preview=True)
+        if welcome_dm_message is not None and welcome_dm_message != "":
+            await bot.send_message(update.effective_user.id, welcome_dm_message, disable_web_page_preview=True)
             logger.info(f"Welcome message sent to user {update.effective_user.id} in chat {update.effective_chat.id} ({update.effective_chat.title})")
 
         chat_join_request = update.chat_join_request
