975f5c17c63268d9fe1ec9d3d5c92e156b8def0a
Nikita Rvachev <nikita@rvachev.org>
2023-09-11 21:11:32 -0400

    Run code on 1st day of month

--- src/cron/scheduler_warn_and_kick_inactive.sh
+++ src/cron/scheduler_warn_and_kick_inactive.sh
@@ -8,9 +8,9 @@ day=$(date +%d)
 echo "Current day is: $day"
 
 # If the current day is 1, start the Python script
-if [ $day -eq 12 ]; then
+if [ $day -eq 1 ]; then
     echo "Executing the Python script."
     python3 users_warn_and_kick_inactive.py
 else
-    echo "Not the 12th day. Skipping the Python script."
+    echo "Not the 1th day. Skipping the Python script."
 fi
