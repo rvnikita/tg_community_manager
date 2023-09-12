56dca326d938a57da3e62c108c295276cf71f41e
Nikita Rvachev <nikita@rvachev.org>
2023-09-11 20:34:38 -0400

    debug Starting users_warn_and_kick_inactive.py

--- src/cron/scheduler_warn_and_kick_inactive.sh
+++ src/cron/scheduler_warn_and_kick_inactive.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+echo "Starting the scheduler script."
+
 # Get the current day of the month
 day=$(date +%d)
 
+echo "Current day is: $day"
+
 # If the current day is 1, start the Python script
-if [ $day -eq 11 ]; then
+if [ $day -eq 12 ]; then
+    echo "Executing the Python script."
     python3 ./src/cron/users_warn_and_kick_inactive.py
+else
+    echo "Not the 12th day. Skipping the Python script."
 fi
