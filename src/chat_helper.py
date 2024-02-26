4535c880fbc0bfbfca08d477031fbaf61f9d221f
Nikita Rvachev <nikita@rvachev.org>
2024-02-26 17:43:03 -0500

    get_chat_mention should not be async as well

--- src/chat_helper.py
+++ src/chat_helper.py
@@ -376,7 +376,7 @@ async def unpin_message(bot, chat_id, message_id):
 
 
 
-async def get_chat_mention(bot, chat_id: int) -> str:
+def get_chat_mention(bot, chat_id: int) -> str:
     try:
         # Fetch chat details from the Telegram API
         chat_details = await bot.get_chat(chat_id)
