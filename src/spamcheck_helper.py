c07444da8be9d96bc0f8ab556f4a33758e6e57a6
Nikita Rvachev <nikita@rvachev.org>
2024-05-01 22:45:32 -0400

    Get rid of data like 4.549766504729044e-05.

--- src/spamcheck_helper.py
+++ src/spamcheck_helper.py
@@ -61,10 +61,10 @@ def predict_spam(message_text, user_id, chat_id):
 
             if prediction_proba[0][1] >= float(threshold):
                 spam_detected = True
-                logger.info(f"‼️Spam ‼️ Probability: {prediction_proba[0][1]}. Threshold: {threshold}. Message: {message_text}")
+                logger.info(f"‼️Spam ‼️ Probability: {prediction_proba[0][1]:.5f}. Threshold: {threshold}. Message: {message_text}")
             else:
                 spam_detected = False
-                logger.info(f"Not Spam. Probability: {prediction_proba[0][1]}. Threshold: {threshold}. Message: {message_text}")
+                logger.info(f"Not Spam. Probability: {prediction_proba[0][1]:.5f}. Threshold: {threshold}. Message: {message_text}")
 
             return spam_detected
 
