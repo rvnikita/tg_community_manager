997a0d3ed25616382363ce13f9d0d479fed6d76d
Nikita Rvachev <nikita@rvachev.org>
2024-11-15 17:13:59 -0500

    Refactor config_helper.py to load environment variables from .env and use ConfigParser with environment variable interpolation

--- src/config_helper.py
+++ src/config_helper.py
@@ -1,8 +1,18 @@
 import os
 import configparser
+from dotenv import load_dotenv
 
 def get_config():
-    config = configparser.SafeConfigParser(os.environ)
-    config_path = os.path.dirname(__file__) + '/../config/' #we need this trick to get path to config folder
-    config.read(config_path + 'settings.ini')
+    # Load environment variables from .env
+    load_dotenv()
+
+    # Create a ConfigParser object with environment variable interpolation
+    config = configparser.ConfigParser(os.environ)
+
+    # Resolve the path to the settings.ini file
+    config_path = os.path.join(os.path.dirname(__file__), '../config/settings.ini')
+
+    # Read the settings.ini file
+    config.read(config_path)
+
     return config
