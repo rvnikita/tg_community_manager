f777dfc92fa8157c31a52ba6420c25f832687166
Nikita Rvachev <nikita@rvachev.org>
2025-02-08 13:15:27 -0500

    Clarify TODO comment regarding embedding calculation and its potential impact on tg_ai_spamcheck execution

--- src/dispatcher.py
+++ src/dispatcher.py
@@ -669,7 +669,7 @@ async def tg_log_message(update, context):
                 pass
                 # logger.info("No 'forward_origin' in message or sender_user data is missing.")
 
-            #TODO:LOW: Maybe we don't need to calculate embedding and insert it in DB here as we will recalculate it later in tg_ai_spamcheck. But we should be careful as it seems like sometimes tg_ai_spamcheck is not called (or maybe called but not updating the message log in DB is there is something wrong with the probability calculation)
+            #TODO:LOW: Maybe we don't need to calculate embedding and insert it in DB here as we will recalculate it later in tg_ai_spamcheck. But we should be careful as it seems like sometimes tg_ai_spamcheck is not called (or maybe called but not updating the message log in DB is there is something wrong with the probability calculation. That happens if "ai_spamcheck_enabled": false in chat config)
             embedding = openai_helper.generate_embedding(message_content)
 
 
