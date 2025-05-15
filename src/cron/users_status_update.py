9f7952d650c6a1269712e3427a83837ab61d4631
Nikita Rvachev <nikita@rvachev.org>
2025-05-15 16:23:55 -0400

    Refactor status_update function to improve error handling and logging for chat member status retrieval

--- src/cron/users_status_update.py
+++ src/cron/users_status_update.py
@@ -44,62 +44,63 @@ async def chat_name_update():
 async def status_update():
     logger.info("Starting status update cron script")
 
-    updates = []  # A list to store all updates
+    updates = []
 
     with db_helper.connect() as conn:
         with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
-            # Step 1: Retrieve 500 random user IDs.
             sql = "SELECT id FROM tg_user ORDER BY RANDOM() LIMIT 1000"
             cur.execute(sql)
             user_rows = cur.fetchall()
             user_ids = [row['id'] for row in user_rows]
             user_dict = {row['id']: row.get('username', '') for row in user_rows}
 
-            # Step 2: Fetch associated statuses for these users.
             user_status_sql = "SELECT * FROM tg_user_status WHERE user_id = ANY(%s)"
             cur.execute(user_status_sql, (user_ids,))
             user_status_rows = cur.fetchall()
 
             for user_status_row in user_status_rows:
                 user_display = user_dict.get(user_status_row['user_id']) or user_status_row['user_id']
-
+                chat_id = user_status_row['chat_id']
+                user_id = user_status_row['user_id']
                 try:
-                    chat_member = await bot.get_chat_member(user_status_row['chat_id'], user_status_row['user_id'])
+                    chat_member = await bot.get_chat_member(chat_id, user_id)
                     status = chat_member.status
 
-                    # Add to updates only if the status has changed
                     if status != user_status_row['status']:
-                        updates.append((status, user_status_row['user_id'], user_status_row['chat_id']))
-                        logger.info(f"Status change detected for user {'@' if not (isinstance(user_display, int) or user_display.isdigit()) else ''}{user_display} in chat {user_status_row['chat_id']} to {status}")
+                        updates.append((status, user_id, chat_id))
+                        logger.info(f"(get_chat_member) Status change detected for user {'@' if not (isinstance(user_display, int) or user_display.isdigit()) else ''}{user_display} in chat {chat_id} to {status}")
                 except BadRequest as bad_request_error:
                     error_message = str(bad_request_error)
+                    where = f"(get_chat_member, chat_id={chat_id}, user_id={user_id})"
                     if "User not found" in error_message:
-                        # Update the user's status to "User not found"
-                        updates.append(("User not found", user_status_row['user_id'], user_status_row['chat_id']))
+                        updates.append(("User not found", user_id, chat_id))
+                        logger.info(f"{where} User not found: {error_message}")
                     elif "Chat not found" in error_message:
-                        # Log the "Chat not found" error as an informational entry with chat ID
-                        logger.info(f"Chat not found (Chat ID: {user_status_row['chat_id']}): {error_message}")
+                        logger.info(f"{where} Chat not found: {error_message}")
                     elif "Member not found" in error_message:
-                        # Update the user's status to "Member not found"
-                        updates.append(("Member not found", user_status_row['user_id'], user_status_row['chat_id']))
+                        updates.append(("Member not found", user_id, chat_id))
+                        logger.info(f"{where} Member not found: {error_message}")
                     elif "Participant_id_invalid" in error_message:
-                        # Update the user's status to "Participant ID invalid"
-                        updates.append(("Participant ID invalid", user_status_row['user_id'], user_status_row['chat_id']))
+                        updates.append(("Participant ID invalid", user_id, chat_id))
+                        logger.info(f"{where} Participant ID invalid: {error_message}")
+                    elif "Chat_admin_required" in error_message:
+                        logger.info(f"{where} Chat_admin_required - not an error: {error_message}")
                     else:
-                        # If it's another kind of BadRequest, log it as an error
-                        logger.error(f"BadRequest error: {bad_request_error}. ChatID: {user_status_row['chat_id']}, UserID: {user_status_row['user_id']}")
+                        logger.error(f"{where} BadRequest error: {traceback.format_exc()}")
                 except Forbidden as forbidden_error:
-                    # Log the Forbidden error as an informational entry with chat ID
-                    logger.info(f"Bot is not a member of the group chat (Chat ID: {user_status_row['chat_id']}): {forbidden_error}")
-                except Exception as error:
-                    logger.error(f"Error fetching chat member status: {traceback.format_exc()}")
+                    where = f"(get_chat_member, chat_id={chat_id}, user_id={user_id})"
+                    logger.info(f"{where} Forbidden: {forbidden_error}")
+                except Exception:
+                    where = f"(get_chat_member, chat_id={chat_id}, user_id={user_id})"
+                    logger.error(f"{where} Exception: {traceback.format_exc()}")
 
-            # Batch update all statuses
             if updates:
                 user_update_sql = "UPDATE tg_user_status set status = %s WHERE user_id = %s AND chat_id = %s"
                 cur.executemany(user_update_sql, updates)
 
             conn.commit()
+
+
 
 
 async def main() -> None:
