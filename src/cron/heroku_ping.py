3112998f215952074986b21a563871339b0da6da
Nikita Rvachev <nikita@rvachev.org>
2025-05-23 17:32:40 -0400

    Increase timeout for conversation in health check to improve reliability

--- src/cron/heroku_ping.py
+++ src/cron/heroku_ping.py
@@ -38,7 +38,7 @@ async def health_check():
     logger.info(f"Logged in as {me.username} (id={me.id})")
 
     try:
-        async with client.conversation(os.getenv("ENV_BOT_USERNAME"), timeout=10) as conv:
+        async with client.conversation(os.getenv("ENV_BOT_USERNAME"), timeout=20) as conv:
             await conv.send_message("/ping")
             resp = await conv.get_response()
             text = resp.raw_text or ""
