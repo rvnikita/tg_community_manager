3a93b45439b628a40ccdd90e82671a96d509dcdd
Nikita Rvachev <nikita@rvachev.org>
2026-01-15 18:38:39 -0500

    update spam model

--- src/cron/antispam_ml_optimized.py
+++ src/cron/antispam_ml_optimized.py
@@ -93,6 +93,7 @@ async def train_spam_classifier():
                         )
                     )
                     .order_by(db_helper.Message_Log.id.desc())
+                    .limit(5000)
             )
             messages_data = query.all()
             query_time = time.time() - start_time
